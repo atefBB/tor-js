573f2af09666489a90c05d6764e78b1b3edb244b
Atef Ben Ali <atef.bettaib@gmail.com>
2018-08-08 11:52:31 +0100

    feat: renew `tor` session

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ var tr = require('tor-request');
 
 tr.TorControlPort.password = 'giraffe';
 
-tr.request('https://api.ipify.org', function (err, res, body) {
-  if (!err && res.statusCode == 200) {
-    console.log("Your public (through Tor) IP is: " + body);
-  } else {
-    console.log("There's an issue " + err);
-  }
+tr.renewTorSession(function(err, success) {
+    if (err) {
+    	console.log("There's an error:" + err);
+    	return;
+    }
+
+    console.log("Session renewed with success!");
+});
+
+tr.request('https://api.ipify.org', function(err, res, body) {
+    if (!err && res.statusCode == 200) {
+        console.log("Your public (through Tor) IP is: " + body);
+    } else {
+        console.log("There's an issue " + err);
+    }
 });
